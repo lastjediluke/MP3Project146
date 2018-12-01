9c5609f2e2bac69d083b19a34f1fa0a7c74c9558
Luke <33014158+lastjediluke@users.noreply.github.com>
2018-12-01 15:13:04 -0800

    go to line 76: notes for Frank

--- main.cpp
+++ main.cpp
@@ -73,18 +73,27 @@ char * artists[10] = {};
 uint8_t artistCounter = 1;
 uint8_t artistSize = 2;
 
+/*	### Notes ###
+1. Get artist and song title from the mp3 files 
+and store it in the artistStruct (maybe we can 
+expand the struct to get album title, length of song, etc.)
+2. Make a function to check if artist already exists in the struct.
+3. Look into how we mmight be able to dynamically size some of these arrays.
+
+
+*/
 struct artistStruct {
 	char *nameOfArtist;
-	char * artistSongs[3];
-	int songCount;
+	char * artistSongs[3];		// I set it up to be 3, we should probably make it larger or figure out how to dynamically size it
+	int songCount;				// this variable stores how many songs the artist has
 };
 
-artistStruct myArtists[4] = {
-	{"Dre", {"Forgot About Dre", "Fuck You", NULL}, 2},
-	{"Weezer", {"Say it Ain't So", "", NULL}, 1},
-	{"", {"", "", ""}, -1}
+artistStruct myArtists[4] = {	// I set it up to be 4, it should be larger or we need to use realloc, calloc, or malloc or something
+	{"Dre", {"Forgot About Dre", "Fuck You", NULL}, 2},		// these are all just sample entries for the struct
+	{"Weezer", {"Say it Ain't So", "", NULL}, 1},			// in a real case, the struct will be filled with what it reads from the mp3
+	{"", {"", "", ""}, -1}									// empty struct to indicate end of the array
 };
-artistStruct *artistStructPtr;
+artistStruct *artistStructPtr;	// this is used in some functions below
 
 // ### Songs ###
 
