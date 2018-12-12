c1722ff2fb0fce61fc79dca49d8ffab559e581e3
bionia <oniabiel205@yahoo.com>
2018-12-11 16:42:59 -0800

    changed artist and song displays to just the filename

--- mp3/L5_Application/Flash.h
+++ mp3/L5_Application/Flash.h
@@ -1,3 +1,31 @@
+// #pragma once
+// #ifndef FLASH_H
+// #define FLASH_H
+
+// #include "queue.h"
+
+// #include "vector.hpp"
+// #include "str.hpp"
+
+// class Flash {
+//     private:
+//         // for dislay output
+//         char mp3Files[10][20];
+//         uint8_t numFiles;
+//     public:
+//         Flash();
+//         // to be used with LCD
+//         void mp3_init(char *songs, char *artists, uint8_t rows, uint8_t cols);
+//         void get_mp3_files();
+//         void get_mp3_metadata(char *songs, char *artists, uint8_t rows, uint8_t cols);
+//         uint8_t get_number_of_songs();
+//         // if user is ready to switch, or start, the song,
+//         // have user specify the song to send to decoder with
+//         // file number (index inside the vector)
+//         bool send_mp3_file_2_decoder(uint8_t songIndex, QueueHandle_t *queue);
+// };
+// #endif
+
 #pragma once
 #ifndef FLASH_H
 #define FLASH_H
@@ -7,21 +35,51 @@
 #include "vector.hpp"
 #include "str.hpp"
 
+#include "VS1011Drv.hpp"
+struct FileName {
+    char buffer[20];
+};
+
+struct SongStruct
+{
+    str songName;
+    str artistName;
+    str fileName;
+};
+struct ArtistStruct 
+{
+    str artistName;
+    VECTOR<SongStruct> * songs;			
+};
+
 class Flash {
     private:
-        // for dislay output
-        char mp3Files[10][20];
-        uint8_t numFiles;
-    public:
         Flash();
+        // for dislay output
+
+        static Flash * m_instance;
+    public:
+        static Flash * instance();
         // to be used with LCD
-        void mp3_init(char *songs, char *artists, uint8_t rows, uint8_t cols);
+        void mp3_init();
         void get_mp3_files();
-        void get_mp3_metadata(char *songs, char *artists, uint8_t rows, uint8_t cols);
+        void get_mp3_metadata(int k, ArtistStruct &artist, SongStruct &song);
         uint8_t get_number_of_songs();
         // if user is ready to switch, or start, the song,
         // have user specify the song to send to decoder with
         // file number (index inside the vector)
-        bool send_mp3_file_2_decoder(uint8_t songIndex, QueueHandle_t *queue);
+        void send_mp3_file_2_decoder();
+
+        void setQueueHandlers(QueueHandle_t &decoderQueue, QueueHandle_t &songQueue);
+        QueueHandle_t * songQueue;
+        QueueHandle_t * decoderQueue;
+
+        char mp3Files[10][20];
+        // char songTitles[10][20];
+        // char artists[10][20];
+        // for opening and reading files
+        VECTOR<FileName> mp3Files1;
+        uint8_t numFiles;
+        PlayBackControl * itsPlayBackControl;
 };
 #endif
